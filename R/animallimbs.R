b64bc5612715e0b301b55ab542dc4ed588919ead
afrikaniz3d-za <afrikaniz3d@gmail.com>
2026-05-20 17:53:09 +0200

    cleaned

--- R/animallimbs.R
+++ R/animallimbs.R
@@ -2,5 +2,5 @@ animal_limbs <- function(animal, legs, wings) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(legs) & length(legs) == 1)
   stopifnot(is.character(wings) & length(wings) == 1)
-  paste0("The ", animal, " has ", legs, "and ", wings, ".")
+  paste0("The ", animal, " has ", legs, " and ", wings, "wings.")
 }
